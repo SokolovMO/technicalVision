ec09051205cdd95b9ca1a28af42efa7eed49e8f5
Nikita Loabzev <pavilich99@mail.ru>
2022-10-20 14:31:25 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -4,15 +4,17 @@ import numpy as np
 def nothing(x):
     pass
 
-picture = cv2.imread('lr4/prikoldesy.jpg')
+picture = cv2.imread('lr4/picture.jpg')
 
 while True:
-    cv2.imshow('prikoldesy', picture)
+    cv2.imshow('picture', picture)
     if cv2.waitKey(1) == ord('q'):
         break
 
+cv2.destroyWindow('picture')
+
 cv2.namedWindow("conversion parameter")
-cv2.createTrackbar("iterations", "conversion parameter", 1, 10, nothing)
+cv2.createTrackbar("iterations", "conversion parameter", 0, 10, nothing)
 cv2.moveWindow("conversion parameter", 0, 800)
 cv2.resizeWindow("conversion parameter", 500, 50)
 
@@ -23,10 +25,30 @@ while True:
     iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
 
     pictureDilate = cv2.dilate(picture, kernel, iterations=iter4tions)
-    cv2.imshow('prikoldesyDilate', pictureDilate)
+    cv2.imshow('pictureDilate', pictureDilate)
     if cv2.waitKey(1) == ord('q'):
         break
 
-cv2.imwrite('lr4/prikoldesyDilate.jpg', pictureDilate)
+cv2.imwrite('lr4/pictureDilate.jpg', pictureDilate)
+cv2.destroyWindow('pictureDilate')
 
-# pictureMorphologyEx = cv2.morphologyEx(picture,
+while True:
+
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+
+    pictureErode = cv2.erode(picture, kernel, iterations=iter4tions)
+    cv2.imshow('pictureErode', pictureErode)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.imwrite('lr4/pictureErode.jpg', pictureErode)
+cv2.destroyWindow('pictureErode')
+
+while True:
+
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+
+    pictureMorphologyEx = cv2.morphologyEx(picture, cv2.MORPH_OPEN, kernel, iterations=iter4tions)
+    cv2.imshow('pictureMorphologyEx', pictureMorphologyEx)
+    if cv2.waitKey(1) == ord('q'):
+        break
