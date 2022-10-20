8379fbdc133767cd4f4e6007e8e7fd9c3bb3de40
Nikita Loabzev <pavilich99@mail.ru>
2022-10-20 16:12:21 +0300

    f

--- lab4.py
+++ lab4.py
@@ -4,6 +4,12 @@ import numpy as np
 def nothing(x):
     pass
 
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
+    pictureDilate = cv2.dilate(picture, kernel, iterations=iter4tions)
+
+
 picture = cv2.imread('lr4/picture.jpg')
 
 while True:
@@ -14,16 +20,17 @@ while True:
 cv2.destroyWindow('picture')
 
 cv2.namedWindow("conversion parameter")
-cv2.createTrackbar("iterations", "conversion parameter", 0, 10, nothing)
+cv2.createTrackbar("iterations", "conversion parameter", 1, 10, nothing)
+cv2.createTrackbar("kernelSize", "conversion parameter", 1, 10, nothing)
+
 cv2.moveWindow("conversion parameter", 0, 800)
 cv2.resizeWindow("conversion parameter", 500, 50)
-
-kernel = np.ones((3, 3), 'uint8')
 
 while True:
 
     iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
-
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
     pictureDilate = cv2.dilate(picture, kernel, iterations=iter4tions)
     cv2.imshow('pictureDilate', pictureDilate)
     if cv2.waitKey(1) == ord('q'):
@@ -35,7 +42,8 @@ cv2.destroyWindow('pictureDilate')
 while True:
 
     iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
-
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
     pictureErode = cv2.erode(picture, kernel, iterations=iter4tions)
     cv2.imshow('pictureErode', pictureErode)
     if cv2.waitKey(1) == ord('q'):
@@ -44,11 +52,77 @@ while True:
 cv2.imwrite('lr4/pictureErode.jpg', pictureErode)
 cv2.destroyWindow('pictureErode')
 
+# OPEN
+
 while True:
 
     iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
-
-    pictureMorphologyEx = cv2.morphologyEx(picture, cv2.MORPH_OPEN, kernel, iterations=iter4tions)
-    cv2.imshow('pictureMorphologyEx', pictureMorphologyEx)
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
+    pictureMORPH_OPEN = cv2.morphologyEx(picture, cv2.MORPH_OPEN, kernel, iterations=iter4tions)
+    cv2.imshow('pictureMORPH_OPEN', pictureMORPH_OPEN)
     if cv2.waitKey(1) == ord('q'):
         break
+
+cv2.imwrite('lr4/pictureMORPH_OPEN.jpg', pictureMORPH_OPEN)
+cv2.destroyWindow('pictureMORPH_OPEN')
+
+# CLOSE
+
+while True:
+
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
+    pictureMORPH_CLOSE = cv2.morphologyEx(picture, cv2.MORPH_CLOSE, kernel, iterations=iter4tions)
+    cv2.imshow('pictureMORPH_CLOSE', pictureMORPH_CLOSE)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.imwrite('lr4/pictureMORPH_CLOSE.jpg', pictureMORPH_CLOSE)
+cv2.destroyWindow('pictureMORPH_CLOSE')
+
+# GRADIENT
+
+while True:
+
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
+    pictureMORPH_GRADIENT = cv2.morphologyEx(picture, cv2.MORPH_GRADIENT, kernel, iterations=iter4tions)
+    cv2.imshow('pictureMORPH_GRADIENT', pictureMORPH_GRADIENT)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.imwrite('lr4/pictureMORPH_GRADIENT.jpg', pictureMORPH_GRADIENT)
+cv2.destroyWindow('pictureMORPH_GRADIENT')
+
+# TOPHAT
+
+while True:
+
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
+    pictureMORPH_TOPHAT = cv2.morphologyEx(picture, cv2.MORPH_TOPHAT, kernel, iterations=iter4tions)
+    cv2.imshow('pictureMORPH_TOPHAT', pictureMORPH_TOPHAT)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.imwrite('lr4/pictureMORPH_TOPHAT.jpg', pictureMORPH_TOPHAT)
+cv2.destroyWindow('pictureMORPH_TOPHAT')
+
+# BLACKHAT
+
+while True:
+
+    iter4tions = int(cv2.getTrackbarPos("iterations", "conversion parameter"))
+    kernelSize = int(cv2.getTrackbarPos("kernelSize", "conversion parameter"))
+    kernel = np.ones((kernelSize, kernelSize), 'uint8')
+    pictureMORPH_BLACKHAT = cv2.morphologyEx(picture, cv2.MORPH_BLACKHAT, kernel, iterations=iter4tions)
+    cv2.imshow('pictureMORPH_BLACKHAT', pictureMORPH_BLACKHAT)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.imwrite('lr4/pictureMORPH_BLACKHAT.jpg', pictureMORPH_BLACKHAT)
+cv2.destroyWindow('pictureMORPH_BLACKHAT')
